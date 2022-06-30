7a39b45f768f69c5e5fa6a3e5dbcf12a1faa7c1a
Mateus Borges <mateusguimaraes717@gmail.com>
2022-06-30 10:39:11 -0300

    javascript pronto

--- js/script.js
+++ js/script.js
@@ -45,3 +45,27 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// animaçao
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
